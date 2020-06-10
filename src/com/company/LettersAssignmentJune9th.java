8037ae1684a6347ab3822f3d4865fb67bfc19f7d
shreeya-madhavanur <madhavanurs23@greenhill.org>
2020-06-10 17:20:23 -0500

    Added more comments and made it neater because I was bored and thought it looked messy

--- src/com/company/LettersAssignmentJune9th.java
+++ src/com/company/LettersAssignmentJune9th.java
@@ -3,19 +3,21 @@ package com.company;
 public class LettersAssignmentJune9th {
     //This is the global variable for sizing
     public static final int SIZE = 6;
-//This is the main method
+    //This is the main method
     public static void main(String[] args) {
-        for (int LimitForLetters = SIZE; LimitForLetters >= 1; LimitForLetters--) {
-        for (int let=0; let<LimitForLetters; let++){
+    for (int LimitForLetters = SIZE; LimitForLetters >= 1; LimitForLetters--) {
+    for (int let=0; let<LimitForLetters; let++){
+    System.out.print((char)(65+let));
+        }
+    //This space is to make sure it prints in a new line
+    System.out.println(" ");
+        }
+    for (int LimitForLetters = 1; LimitForLetters<=SIZE; LimitForLetters++){
+    for (int let = 0; let<LimitForLetters; let++){
             System.out.print((char)(65+let));
         }
-        System.out.println(" ");
-        }
-        for (int LimitForLetters = 1; LimitForLetters<=SIZE; LimitForLetters++){
-        for (int let = 0; let<LimitForLetters; let++){
-            System.out.print((char)(65+let));
-        }
-        System.out.println(" ");
+    //This space is to make sure the next thing (if i wanted to make one) would be printed on a new line
+    System.out.println(" ");
         }
     }
 }
