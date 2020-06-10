16074601db1b99651d848d26fb5b3893a68901e3
shreeya-madhavanur <madhavanurs23@greenhill.org>
2020-06-10 12:29:01 -0500

    Added Comments

--- src/com/company/LettersAssignmentJune9th.java
+++ src/com/company/LettersAssignmentJune9th.java
@@ -1,8 +1,9 @@
 package com.company;
 
 public class LettersAssignmentJune9th {
+    //This is the global variable for sizing
     public static final int SIZE = 6;
-
+//This is the main method
     public static void main(String[] args) {
         for (int LimitForLetters = SIZE; LimitForLetters >= 1; LimitForLetters--) {
         for (int let=0; let<LimitForLetters; let++){
