04f5434c0cd07fff56a872820a3a608c4549ebb7
shreeya-madhavanur <madhavanurs23@greenhill.org>
2020-06-15 16:04:55 -0500

    Final version 6/15/20 This program finds the number of unique characters in the first word that are present in the entire phrase. Added Pseudocode.

--- src/com/company/June15thletterloopfinal.java
+++ src/com/company/June15thletterloopfinal.java
@@ -1,6 +1,33 @@
+//Pseudocode:
+//1. Create the scanner object
+//2. Print a message for the user
+//3. Create string object for the message
+//4. Get the message using nextLine()
+//5. Print System.out.print (“Type your word or phrase here:”) to get the user input
+//6. Find the first word
+//7. Int indexOfSpace= firstWord.indexOf(“ “)
+//8. Find length of first word
+//9. Also isolate the message
+//10. With index (indexOf())
+//11. Counter = 0;
+//12. For loop control by length of the first word	index = 1
+//13. Letter → charAt(indexloop)
+//14. Count++ While index>-1 repeat the following loop When index = 0, use that letter and make it ++
+//15. Check if you have the same letter in the first word in the message
+//              a. for (int i = 0; i < someString.length(); i++) {
+//                 if (someString.charAt(i) == someChar) {
+//                 count++;  }
+//16. If it is true
+//17. Count++
+//18. System.out.println string ( “The letter “ + charAt(whatever index it is) + “appears in the message ” + Count++ answer + “times.”
+//19. Eliminate that letter from the first word
+//20. First word = first word.substring(0, index you found your second letter) + first word.substring (index you found your second letter +1)
+//21. Use while to repeat the process
+
 package com.company;
 
 import java.util.Scanner;
+
 
 public class June15thletterloopfinal {
     //This is the Main Method
