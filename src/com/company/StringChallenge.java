4b758628263287667d77aba3672df07b2de17c63
shreeya-madhavanur <madhavanurs23@greenhill.org>
2020-06-19 18:38:20 -0500

    Conditionals Challenge 6/18 --> contains pseudocode, code, and comments --> updated to get rid of unnecessary code --> Updated to have a random

---
+++ src/com/company/StringChallenge.java
@@ -0,0 +1,52 @@
+/* Shreeya Madhavanur
+Psuedocode:
+1. Import java.util*
+2. make scanner object thing --> Scanner screen = new Scanner(System.in);
+Main:
+    7. Prompt for T
+    8. Do while loop for T
+        a. As long as t <1 or t>10
+        b. println "T must be between 1 and 10 inclusive, try again"
+        c. define the int T as the answer (screen.nextLine() thing)
+    9. For loop controlled by T
+        a. Prompt for a word
+        b. declare the String = call Method(new word)
+        c. Println new word
+Return Method:
+    10. String EvenLetter1
+    11. String OddLetter1
+    12. String EvenLetter2
+    13. String OddLetter2
+    14. String Length of EvenLetter1 = N1
+    15. String Length of EvenLetter2 = N2
+    16. String Length of OddLetter1 = N3
+    17. String Length of OddLetter2 = N4
+    18. int p = 0
+    19. While loop with index control
+        a. While N1 >= 0
+        b. Print charAt index p for word1
+        c. int p = p+2
+    20. Print "  "
+    21. int q = 1
+    22. While loop with index control
+        a. While N2 >= 0
+        b. Print charAt index p for word1
+        c. int q = q+1
+    23. println " "
+    24. While loop with index control
+        a. While N3 >= 0
+        b. Print charAt index p for word1
+        c. int p = p+2
+
+ */
+
+package com.company;
+
+import java.util.Scanner;
+
+
+public class StringChallenge {
+    //This is the Main Method
+    public static void main(String[] args) {
+    }
+}
