f483bccebe4521f737490fb84086082b26daac64
shreeya-madhavanur <madhavanurs23@greenhill.org>
2020-06-19 18:39:27 -0500

    Conditionals Challenge 6/18 --> contains pseudocode, code, and comments --> updated to get rid of unnecessary code --> Updated to have a random

--- src/com/company/IntroToConditionalStatementsChallenge.java
+++ src/com/company/IntroToConditionalStatementsChallenge.java
@@ -1,58 +1,61 @@
 /* Shreeya Madhavanur
 Psuedocode:
 1. Import java.util*
-2. make scanner object thing --> Scanner screen = new Scanner(System.in);
-3. prompt for integer between 1 and 137 inclusive
-4. define the integer n as the answer (screen.nextInt thing)
-5. make a while loop containing the if statement for if the integer is above 137
-    a. print that the integer is invalid and must be less than or equal to 137 + try again
-    b. define their input response as the new n
-6. make a while loop containing the if statement for if the integer is below 1
-    a. print that the integer is invalid and must be greater than or equal to 1 + try again
-    b. define their input response as the new n
-7. define the integer remainder as the n module 2
-8. create an if statement for if the remainder is one OR if it has a remainder 0 and is between 6-20 inclusive
-    a. println weird
-9. create an else if statement for if the number has remainder 0 and is between 2-5 inclusive OR if it has remainder 0 and is greater than or equal to 20
-    a. println Not weird
+2. make scanner object thing --> Scanner input = new Scanner(System.in);
+3. Make the main method
+4. define n as a random
+5. Create the String Answer
+6. Create a while loop
+    a. define new int integer as a random n within 1-137 inclusive
+    b. println the integer
+    c. insert output method
+    d. Ask the user if they want to keep going
+    e. define the string "answer" as the answer
+7. Repeat loop while answer equals "Yes"
+8. Create output method
+        1. define the integer remainder as the n module 2
+        2. create an if statement for if the remainder is one OR if it has a remainder 0 and is between 6-20 inclusive
+            a. println weird
+        3. create an else if statement for if the number has remainder 0 and is between 2-5 inclusive OR if it has remainder 0 and is greater than or equal to 20
+            a. println Not weird
+
 
  */
 
 package com.company;
 
-import java.util.Scanner;
+import java.util.*;
 
-
+//This is the class
 public class IntroToConditionalStatementsChallenge {
-    //This is the Main Method
-    public static void main(String[] args) {
-       //User control
-        Scanner screen = new Scanner(System.in);
-        System.out.println("Type in an integer between 1 and 137 inclusive:");
-        int n = screen.nextInt();
-      /*i had to do this weird if statement within a while loop because otherwise my program wouldn't work and acted
-      erratically. I don't exactly know why, but putting an if loop within the while loop made my program work again.*/
-        do{ if (n > 137){
-           System.out.println("Your integer needs to be LESS than or EQUAL TO 137; try again");
-           n = screen.nextInt();
-       }}while (n > 137);
-
-       do{ if(n < 1){
-           System.out.println("Your integer needs to be GREATER than or EQUAL TO 1; try again");
-           n = screen.nextInt();
-       }}while (n < 1);
-    //providing the output
-        int remainder = n%2;
-        if ((remainder == 1) || ( n>=6 && n<=20)){
-            System.out.println("Weird");
+    //This is the scanner object
+    public static Scanner input = new Scanner(System.in);
+    //This is the main
+    public static void main(String[] args){
+        //This is the random defined as n
+        Random n = new Random();
+        String answer;
+        do{
+            //the this defines the random as a bounded integer from 1-137 inclusive
+            int integer = n.nextInt(137) + 1;
+            System.out.println(integer);
+            //This is the output integer which is listed below
+            output(integer);
+            //This is the user prompt
+            System.out.println("Do you want to keep going? Type \"Yes\" or \"No\"");
+            answer = input.next();
         }
-        else if (( n>=2 && n<=5)||(n>=20))
-    {
+        while (answer.equals("Yes"));
+
+    }
+    //this method determines the output
+    public static void output(int integer){
+        int remainder = integer % 2;
+        if ((remainder == 1) || (integer >= 6 && integer <= 20)) {
+            System.out.println("Weird");
+        } else if ((integer >= 2 && integer <= 5) || (integer >= 20)) {
             System.out.println("Not Weird");
         }
 
-        }
-
     }
-
-
+}
